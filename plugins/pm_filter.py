8146b57048d152c26184c687edc14c120c90d5b2
Unkmown24680 <144600694+Unkmown24680@users.noreply.github.com>
2023-11-12 01:31:41 +0530

    Update pm_filter.py

--- plugins/pm_filter.py
+++ plugins/pm_filter.py
@@ -247,7 +247,7 @@ async def next_page(bot, query):
         InlineKeyboardButton("! Lᴀɴɢᴜᴀɢᴇs !", callback_data=f"select_lang#{req}")
     ])
     btn.insert(0, [
-        InlineKeyboardButton("🤔 Hᴏᴡ Tᴏ Dᴏᴡɴʟᴏᴀᴅ 🤔", url=f"https://t.me/+CRxXz6AAWTQ4ZWFl")
+        InlineKeyboardButton("🤔 Hᴏᴡ Tᴏ Dᴏᴡɴʟᴏᴀᴅ 🤔", url=f"https://t.me/movierulzking")
     ])
     try:
         await query.edit_message_reply_markup(
