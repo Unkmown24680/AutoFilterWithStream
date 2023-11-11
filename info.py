77eac9b8169846c2e7a19236069e4e12a55f1ca2
Unkmown24680 <144600694+Unkmown24680@users.noreply.github.com>
2023-11-11 13:04:57 +0530

    Update info.py

--- info.py
+++ info.py
@@ -44,7 +44,7 @@ NO_RESULTS_MSG = is_enabled((environ.get("NO_RESULTS_MSG", 'False')), False)
 
 # MongoDB information
 DATABASE_URI = environ.get('DATABASE_URI', "mongodb+srv://kipaw96209:a0CLZ056nocL9tO4@cluster0.znbnxyq.mongodb.net/?retryWrites=true&w=majority")
-DATABASE_NAME = environ.get('DATABASE_NAME', "Mahi")
+DATABASE_NAME = environ.get('DATABASE_NAME', "mahi")
 COLLECTION_NAME = environ.get('COLLECTION_NAME', 'Mahesh')
 
 # Others
