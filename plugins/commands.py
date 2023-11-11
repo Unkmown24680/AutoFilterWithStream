fd6e01e1d1c3215f4c4845b7a814dccbcd1e8874
Unkmown24680 <144600694+Unkmown24680@users.noreply.github.com>
2023-11-11 13:16:32 +0530

    Update commands.py

--- plugins/commands.py
+++ plugins/commands.py
@@ -51,16 +51,13 @@ async def start(client, message):
         await client.send_message(LOG_CHANNEL, script.LOG_TEXT_P.format(message.from_user.id, message.from_user.mention))
     if len(message.command) != 2:
         buttons = [[
-                    InlineKeyboardButton('Aᴅᴅ Mᴇ Tᴏ Yᴏᴜʀ Gʀᴏᴜᴘ', url=f'http://t.me/{temp.U_NAME}?startgroup=true')
+                    InlineKeyboardButton('➕Aᴅᴅ Mᴇ Tᴏ Yᴏᴜʀ Gʀᴏᴜᴘ➕', url=f'http://t.me/{temp.U_NAME}?startgroup=true')
                 ],[                                     
                     InlineKeyboardButton('Loot Deals⚡🤩', url="https://t.me/lootdealsstech"), 
                     InlineKeyboardButton('Movie Group📽️', url="https://t.me/anymovierequesthere")
-                ],[                                         
-                    InlineKeyboardButton('Fᴇᴀᴛᴜʀᴇs💎', callback_data='help'), 
-                    InlineKeyboardButton('Aʙᴏᴜᴛ🏝️', callback_data='about')
-
-                ],[                                        
-                    InlineKeyboardButton('💎Join Backup🛡️', url="t.me/movierulzking")                            
+                ],[                                     
+                    InlineKeyboardButton('Sᴜᴘᴘᴏʀᴛ👀', url="https://t.me/lootdealsstech"), 
+                    InlineKeyboardButton('Owner😶‍🌫️', url="https://t.me/sm_contactadmin_bot")
                 ]]
         
         reply_markup = InlineKeyboardMarkup(buttons)
